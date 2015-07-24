cc0d19ee5242bc37ef83ab899eaab6f5294bc6d9
JuneLee2015 <zanjli2012@gmail.com>
2015-07-24 14:02:13 +0800

    commit,控制查询开始日期不能大于结束日期

--- HaiMaApp.Web/Scripts/lee-1.0.js
+++ HaiMaApp.Web/Scripts/lee-1.0.js
@@ -15,11 +15,15 @@ lee.common = {
             return "";
         else
             return decodeURIComponent(results[1].replace(/\+/g, " "));
+    },
+    compareDate: function (d1, d2) {  // 时间比较的方法，如果d1时间比d2时间大，则返回true   
+        return Date.parse(d1.replace(/-/g, "/")) > Date.parse(d2.replace(/-/g, "/"))
     }
 }
 
+
 $(function () {
-   
+
     $(".goback").click(function () { window.history.go(-1); });
 
 })
